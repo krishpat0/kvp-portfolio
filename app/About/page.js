04d5dc61854cea039af3d12c5f3ae722879a3e2c
krishpat0 <krishpat001800@gmail.com>
2023-08-22 17:59:29 +0530

    change font

--- app/About/page.js
+++ app/About/page.js
@@ -3,7 +3,9 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <div id="about-page">About</div>
+      <div id="about-page" className="font-bold">
+        About
+      </div>
     </>
   );
 };
