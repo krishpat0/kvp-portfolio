afe9d3c9a793cabf00429b61254bfdce94d489fb
krishpat0 <krishpat001800@gmail.com>
2023-08-23 11:09:06 +0530

    change in tailwind css

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -4,10 +4,17 @@ Link;
 const Header = () => {
   return (
     <>
-      <div id="Header" className="">
-        <h2 id="logo">Krishna</h2>
-        <div id="nav" className="">
-          <Link href="./About">About</Link>
+      <div
+        id="Header"
+        class="flex justify-between  text-white bg-zinc-800 h-10 items-center flex-wrap px-5"
+      >
+        <h2 id="logo" class="text-2xl">
+          Krishna
+        </h2>
+        <div id="nav" class="flex gap-5">
+          <Link href="./About" class="cursor-progress">
+            About
+          </Link>
           <Link href="./Product">Product</Link>
           <Link href="./Course">Course</Link>
         </div>
